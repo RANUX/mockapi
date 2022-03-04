bd56d3e736a92ee9ea0b53f8e66a0b0aae8ff085
Alexander Razzhivin <admin@docode.ru>
2022-03-04 21:16:28 +0300

    change scanner url

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('mocks.urls')),
+    path('scanner/', include('mocks.urls')),
     path('admin/', admin.site.urls),
 ]
