724fdb43a6e4904e0b843cf16cd89ec4d7df80e2
Alexander Razzhivin <admin@docode.ru>
2022-03-12 22:18:26 +0300

    change field name

--- djangoapp/mocks/models.py
+++ djangoapp/mocks/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class MockData(models.Model):
     status = models.CharField(max_length=10)
     message = models.CharField(max_length=255)
-    data = models.JSONField(null=True, blank=True)
+    data = models.JSONField(null=True, blank=True, verbose_name='Response data property')
     request_data = models.JSONField(null=True, blank=True)
