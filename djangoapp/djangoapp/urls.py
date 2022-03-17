d2a2c2c1843c2886ae66209cbd916784fc89713b
Alexander Razzhivin <admin@docode.ru>
2022-03-17 22:27:48 +0300

    fix url

--- djangoapp/djangoapp/urls.py
+++ djangoapp/djangoapp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('mocks/', include('mocks.urls')),
+    path('api/v1/', include('mocks.urls')),
     path('admin/', admin.site.urls),
 ]
