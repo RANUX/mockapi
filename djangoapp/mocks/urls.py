48e5c577566e2a5a2e5c1ff563c1309bfd250a52
Alexander Razzhivin <admin@docode.ru>
2022-03-17 22:25:17 +0300

    fix

--- djangoapp/mocks/urls.py
+++ djangoapp/mocks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('api/v1/scanner/scanned_code.php', mock_post_request_view)
+    path('api/v1/scanner/scanned_code.php/', mock_post_request_view)
 ]
